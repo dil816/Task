d9bed565d63347c2c29a40985779d73db09f389c
dil816 <Rivindudilochana18329@gmail.com>
2024-05-12 10:21:37 +0530

    view model added

---
+++ app/src/main/java/com/example/task/TaskViewModal.kt
@@ -0,0 +1,31 @@
+package com.example.task
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class TaskViewModal(application: Application) : AndroidViewModel(application) {
+    val allTasks: LiveData<List<Task>>
+    val repository: TaskRepository
+
+    init {
+        val dao = TaskDatabase.getDatabase(application).getTasksDao()
+        repository = TaskRepository(dao)
+        allTasks = repository.allTasks
+    }
+
+    fun deleteTask(task: Task) = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(task)
+    }
+
+    fun updateTask(task: Task) = viewModelScope.launch(Dispatchers.IO) {
+        repository.update(task)
+    }
+
+    fun addTask(task: Task) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(task)
+    }
+}
